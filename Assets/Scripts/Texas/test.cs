ee8c397933f6ae1b38864fb29aea1081abc3559f
itsmeejackk <eexyye@gmail.com>
2025-10-24 21:44:50 +0200

    idfk bro

--- Assets/Scripts/Texas/test.cs
+++ Assets/Scripts/Texas/test.cs
@@ -18,9 +18,24 @@ public class test : MonoBehaviour
     {
         
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             Test0.transform.position += new Vector3(5, 0, 0) * Time.deltaTime;
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Test0.transform.position += new Vector3(-5, 0, 0) * Time.deltaTime;
+                }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Test0.transform.position += new Vector3(0, 50, 0) * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            Test0.transform.position += new Vector3(0, 50, 0) * Time.deltaTime;
+        }
+
+
+
     }
 }
